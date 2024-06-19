64f4aabf65f97b9491ca67c51f951c9a5d50f8ee
Hollman Rojas <hollmanerojas@gmail.com>
2024-06-19 18:50:07 -0500

    Feature/ create videoController and API routes

---
+++ app/Http/Controllers/VideoController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Video;
+use Illuminate\Http\Request;
+
+class VideoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $videos = Video::all();
+        return response()->json($videos);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $video = Video::create($request->all());
+        return response()->json($video, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $video = Video::findOrFail($id);
+        return response()->json($video);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $video = Video::findOrFail($id);
+        $video->update($request->all());
+        return response()->json($video);
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $video = Video::findOrFail($id);
+        $video->delete();
+        return response()->json(['message' => 'Video deleted']);
+    }
+}
